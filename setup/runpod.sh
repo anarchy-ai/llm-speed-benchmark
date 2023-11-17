5216363225a81111edfbdc096db14df9ee367468
mehmet-mhy <mehmet.mhy@gmail.com>
2023-11-17 09:20:45 -0700

    added setup/

---
+++ setup/runpod.sh
@@ -0,0 +1,14 @@
+# Website: https://www.runpod.io/console/pods
+# Service: RunPod
+
+apt update
+apt install vim
+pip3 install transformers
+pip3 install --ignore-installed llm-vm
+pip3 install "weaviate-client==3.*"
+pip3 install pinecone-client
+pip3 install python-dotenv
+pip3 install protobuf
+echo
+huggingface-cli login
+
