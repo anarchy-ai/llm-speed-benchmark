61164dccccd1141a2ff0beea4b59c5ddd6f1f929
Mehmet <mehmet.mhy@gmail.com>
2023-11-20 03:00:39 -0700

    cleaned the code

--- hardware.py
+++ hardware.py
@@ -11,6 +11,37 @@ November 20, 2023
 import psutil
 import platform
 from datetime import datetime
+import GPUtil
+from tabulate import tabulate
+import subprocess
+import sys
+import json
+
+def execute(cmd):
+    proc = subprocess.Popen(str(cmd), shell=True, stdout=subprocess.PIPE,)
+    output = proc.communicate()[0].decode("utf-8")
+    return output.split("\n")
+
+def neofetch():
+    # requires neofetch (https://github.com/dylanaraps/neofetch)
+    cmd = "neofetch --stdout"
+    output = {}
+    try:
+        stdout = execute(cmd)
+        for line in stdout:
+            if ": " not in line:
+                continue
+            tmp = line.split(": ", 1)
+            if len(tmp) != 2:
+                continue
+            key = tmp[0].lower().strip()
+            value = tmp[1].strip()
+            output[key] = value
+    except:
+        pass
+    return output
+
+########################################################################################
 
 def get_size(bytes, suffix="B"):
     """
@@ -25,105 +56,109 @@ def get_size(bytes, suffix="B"):
             return f"{bytes:.2f}{unit}{suffix}"
         bytes /= factor
 
-print("="*40, "System Information", "="*40)
-uname = platform.uname()
-print(f"System: {uname.system}")
-print(f"Node Name: {uname.node}")
-print(f"Release: {uname.release}")
-print(f"Version: {uname.version}")
-print(f"Machine: {uname.machine}")
-print(f"Processor: {uname.processor}")
+def system_info():
+    uname = platform.uname()
+    return {
+        "platform": {
+            "System": uname.system,
+            "Node Name": uname.node,
+            "Release": uname.release,
+            "Version": uname.version,
+            "Machine": uname.machine,
+            "Processor": uname.processor
+        },
+        "neofetch": neofetch()
+    }
 
-# Boot Time
-print("="*40, "Boot Time", "="*40)
-boot_time_timestamp = psutil.boot_time()
-bt = datetime.fromtimestamp(boot_time_timestamp)
-print(f"Boot Time: {bt.year}/{bt.month}/{bt.day} {bt.hour}:{bt.minute}:{bt.second}")
+def cpu_info(options={}):
+    model_name = options.get("model")
+    if model_name == None:
+        model_name = neofetch()["cpu"]
+    
+    cores = None
+    if options.get("cores") == True:
+        cores = {}
+        for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
+            cores[str(i)] = f"{percentage}%"
+    
+    cpufreq = psutil.cpu_freq()
 
-# let's print CPU information
-print("="*40, "CPU Info", "="*40)
-# number of cores
-print("Physical cores:", psutil.cpu_count(logical=False))
-print("Total cores:", psutil.cpu_count(logical=True))
-# CPU frequencies
-cpufreq = psutil.cpu_freq()
-print(f"Max Frequency: {cpufreq.max:.2f}Mhz")
-print(f"Min Frequency: {cpufreq.min:.2f}Mhz")
-print(f"Current Frequency: {cpufreq.current:.2f}Mhz")
-# CPU usage
-print("CPU Usage Per Core:")
-for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
-    print(f"Core {i}: {percentage}%")
-print(f"Total CPU Usage: {psutil.cpu_percent()}%")
+    output = {
+        "model": str(model_name),
+        "physical_cores": psutil.cpu_count(logical=False),
+        "total_cores": psutil.cpu_count(logical=True),
+        "max_frequency": f"{cpufreq.max:.2f}Mhz",
+        "min_frequency": f"{cpufreq.min:.2f}Mhz",
+        "current_frequency": f"{cpufreq.current:.2f}Mhz"
+    }
 
-# Memory Information
-print("="*40, "Memory Information", "="*40)
-# get the memory details
-svmem = psutil.virtual_memory()
-print(f"Total: {get_size(svmem.total)}")
-print(f"Available: {get_size(svmem.available)}")
-print(f"Used: {get_size(svmem.used)}")
-print(f"Percentage: {svmem.percent}%")
-print("="*20, "SWAP", "="*20)
-# get the swap memory details (if exists)
-swap = psutil.swap_memory()
-print(f"Total: {get_size(swap.total)}")
-print(f"Free: {get_size(swap.free)}")
-print(f"Used: {get_size(swap.used)}")
-print(f"Percentage: {swap.percent}%")
+    if cores != None:
+        output["cores"] = cores
 
-# Disk Information
-print("="*40, "Disk Information", "="*40)
-print("Partitions and Usage:")
-# get all disk partitions
-partitions = psutil.disk_partitions()
-for partition in partitions:
-    print(f"=== Device: {partition.device} ===")
-    print(f"  Mountpoint: {partition.mountpoint}")
-    print(f"  File system type: {partition.fstype}")
-    try:
-        partition_usage = psutil.disk_usage(partition.mountpoint)
-    except PermissionError:
-        # this can be catched due to the disk that
-        # isn't ready
-        continue
-    print(f"  Total Size: {get_size(partition_usage.total)}")
-    print(f"  Used: {get_size(partition_usage.used)}")
-    print(f"  Free: {get_size(partition_usage.free)}")
-    print(f"  Percentage: {partition_usage.percent}%")
-# get IO statistics since boot
-disk_io = psutil.disk_io_counters()
-print(f"Total read: {get_size(disk_io.read_bytes)}")
-print(f"Total write: {get_size(disk_io.write_bytes)}")
+    return output
 
-#### NOTE: lets NOT include network data/information...
+def memory_info():
+    svmem = psutil.virtual_memory()
+    swap = psutil.swap_memory()
+    return {
+        "total": get_size(svmem.total),
+        "available": get_size(svmem.available),
+        "used": get_size(svmem.used),
+        "percentage": svmem.percent,
+        "swap": {
+            "total": get_size(swap.total),
+            "free": get_size(swap.free),
+            "used": get_size(swap.used),
+            "usage": swap.percent
+        }
+    }
 
-# GPU information
-import GPUtil
-from tabulate import tabulate
-print("="*40, "GPU Details", "="*40)
-gpus = GPUtil.getGPUs()
-list_gpus = []
-for gpu in gpus:
-    # get the GPU id
-    gpu_id = gpu.id
-    # name of GPU
-    gpu_name = gpu.name
-    # get % percentage of GPU usage of that GPU
-    gpu_load = f"{gpu.load*100}%"
-    # get free memory in MB format
-    gpu_free_memory = f"{gpu.memoryFree}MB"
-    # get used memory
-    gpu_used_memory = f"{gpu.memoryUsed}MB"
-    # get total memory
-    gpu_total_memory = f"{gpu.memoryTotal}MB"
-    # get GPU temperature in Celsius
-    gpu_temperature = f"{gpu.temperature} °C"
-    gpu_uuid = gpu.uuid
-    list_gpus.append((
-        gpu_id, gpu_name, gpu_load, gpu_free_memory, gpu_used_memory,
-        gpu_total_memory, gpu_temperature, gpu_uuid
-    ))
+def disk_info():
+    output = {}
+    partitions = psutil.disk_partitions()
+    for partition in partitions:
+        try:
+            partition_usage = psutil.disk_usage(partition.mountpoint)
+        except PermissionError:
+            partition_usage = ""
 
-print(tabulate(list_gpus, headers=("id", "name", "load", "free memory", "used memory", "total memory",
-                                   "temperature", "uuid")))
+        output["partition.device"] = {
+            "mount_point": partition.mountpoint,
+            "file_system_type": partition.fstype,
+            "total_size": get_size(partition_usage.total),
+            "used": get_size(partition_usage.used),
+            "free": get_size(partition_usage.free),
+            "usage": f"{partition_usage.percent}%",
+            "partition_usage": partition_usage
+        }
+    
+    disk_io = psutil.disk_io_counters()
+    return {
+        "total_read": get_size(disk_io.read_bytes),
+        "total_write": get_size(disk_io.write_bytes),
+        "partitions": output
+    }
+
+def gpu_info():
+    gpus = GPUtil.getGPUs()
+    gpus_data = {}
+    for gpu in gpus:
+        gpus_data[gpu.id] = {
+            "raw": vars(gpu),
+            "clean": {
+                "id": gpu.id,
+                "uuid": gpu.uuid,
+                "name": gpu.name,
+                "load": f"{gpu.load*100}%",
+                "memory": {
+                    "free": f"{gpu.memoryFree}MB",
+                    "used": f"{gpu.memoryUsed}MB",
+                    "total": f"{gpu.memoryTotal}MB"
+                },
+                "temp": f"{gpu.temperature} °C",
+
+            }
+        }
+    return gpus_data
+
+
