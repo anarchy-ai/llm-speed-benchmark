4741b7f1e51290a6b9b9c961c17328b77d3f5b58
Mehmet <mehmet.mhy@gmail.com>
2023-11-22 01:18:45 -0700

    updated runpod.sh

--- setup/runpod.sh
+++ setup/runpod.sh
@@ -3,19 +3,13 @@
 
 apt update
 apt install vim
-sudo apt install neofetch
+apt install neofetch
 
 pip3 install transformers
-pip3 install --ignore-installed llm-vm
-pip3 install "weaviate-client==3.*"
-pip3 install pinecone-client
-pip3 install python-dotenv
-pip3 install protobuf
-
 pip3 install psutil
 pip3 install gputil
 pip3 install tabulate
-# pip3 install torch torchvision torchaudio
+pip3 install torch torchvision torchaudio
 
-echo
 huggingface-cli login
+
