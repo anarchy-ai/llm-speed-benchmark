db874a3728f6f95cc6b70fa75ce79adcf710ef73
Mehmet <mehmet.mhy@gmail.com>
2023-11-21 22:45:21 -0700

    added model.py, which will be a cli you can run to run the model for benchmarking

---
+++ model.py
@@ -0,0 +1,81 @@
+import torch
+import time
+import json
+import os
+import sys
+import copy
+import gc
+import signal
+import argparse
+import uuid
+
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "src"))
+import hf
+
+parser = argparse.ArgumentParser(description='run hardware performance/metrics collector')
+
+"""
+November 21, 2023
+The default values and help values for most of these parameters were taken directly from huggingface documentation:
+https://huggingface.co/docs/transformers/main_classes/text_generation#transformers.GenerationConfig
+"""
+parser.add_argument('--max_length', type=int, default=20, help='The maximum length the generated tokens can have. Corresponds to the length of the input prompt + max_new_tokens. Its effect is overridden by max_new_tokens, if also set.')
+parser.add_argument('--temperature', type=float, default=1.0, help='The value used to modulate the next token probabilities.')
+parser.add_argument('--top_k', type=int, default=50, help='The number of highest probability vocabulary tokens to keep for top-k-filtering.')
+parser.add_argument('--top_p', type=float, default=1.0, help='If set to float < 1, only the smallest set of most probable tokens with probabilities that add up to top_p or higher are kept for generation.')
+parser.add_argument('--num_return_sequences', type=int, default=1, help='The number of independently computed returned sequences for each element in the batch.')
+
+parser.add_argument('--uuid', type=str, default=str(uuid.uuid4()), help='The UUID for the collection')
+parser.add_argument('--prompt', type=str, default="Hello World", help='Text prompt for the LLM model to respond too')
+parser.add_argument('--model', type=str, default="", help='Huggingface repo/path to LLM model')
+parser.add_argument('--device', type=str, default="", help='Device to run the model on, this can be "cpu" or "cuda:N"')
+
+# signal handler
+def signal_handler(signum, frame):
+    sys.exit(1)
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGTERM, signal_handler)
+    signal.signal(signal.SIGINT, signal_handler)
+
+    args = parser.parse_args()
+
+    print(f"{args.uuid} - model running - running model with following parameters {str(args)}")
+
+    if args.model == "":
+        sys.exit(1)
+
+    local_hf_exists = hf.get_hf_model(args.model)
+    if local_hf_exists == False:
+        sys.exit(1)
+
+    start_time = time.time()
+    
+    output = hf.run_llm(args.model, args.prompt, args.device, {
+        "max_length": args.max_length,
+        "temperature": args.temperature,
+        "top_k": args.top_k,
+        "top_p": args.top_p,
+        "num_return_sequences": args.num_return_sequences
+    })
+
+    end_time = time.time()
+
+    output["run_period"] = {
+        "started": start_time,
+        "ended": end_time
+    }
+
+    # delete cachue and variables to free up resources for better metrics collecting
+    final_result = copy.deepcopy(output)
+    gc.collect()
+    gc.enable()
+    del output
+    if "cuda" in args.model:
+        torch.cuda.empty_cache()
+
+    filepath = f"{args.uuid}_model.json"
+    with open(str(filepath), "w") as file:
+        json.dump(output, file, indent=4)
+    
+    print(f"{args.uuid} - model running - saved output for model run to file {filepath}")
--- src/hf.py
+++ src/hf.py
@@ -91,38 +91,37 @@ def run_llm(model_name, input, device="", model_params={}):
         }
     }
 
-"""
-NAME:
-    get_hf_model()
-
-ABOUT:
-    Check if hf model exists and download hf model to local disk if needed
-
-OUTPUTS:
-    - return True if hf model was downloaded successfully
-    - return True if hf model is already downloaded on the disk
-    - return False if function failed to download model or an error occurred
-"""
+# Check if hf model exists and download hf model to local disk if needed
 def get_hf_model(hf_repo_path):
     # quick & dirty way to check if a hf model/repo exists
     api = HfApi()
     try:
         api.list_repo_files(hf_repo_path)
     except Exception as err:
-        print(f"failed to check if model {hf_repo_path} exists on HuggingFace due to error: {err}")
-        return False
+        return { 
+            "msg": f"failed to check if model {hf_repo_path} exists on HuggingFace due to error: {err}", 
+            "status": False 
+        }
     
     # quick & dirty way to check if hf model/repo has been downloaded, saved to cache directory
     cache_dir = os.path.join(os.path.expanduser('~'), ".cache/huggingface/hub/")
     model_cache_dir = os.path.join(cache_dir, "models--{}".format(hf_repo_path.replace("/", "--")))
     if os.path.isdir(model_cache_dir):
-        print(f"model {hf_repo_path} already exists in directory {model_cache_dir}")
-        return True
+        return { 
+            "msg": f"model {hf_repo_path} already exists in directory {model_cache_dir}", 
+            "status": True
+        }
     
     # download hf model/repo if it's not downloaded
     try:
         snapshot_download(repo_id=hf_repo_path, repo_type="model", token=True)
-        return True
-    except:
-        return False
+        return { 
+            "msg": f"downloaded model {hf_repo_path}", 
+            "status": True
+        }
+    except Exception as err:
+        return { 
+            "msg": f"{err}", 
+            "status": False 
+        }
 
