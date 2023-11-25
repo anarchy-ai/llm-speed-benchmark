7ef67c5550abb06b7f1a23cd6d05ce3db1a02ec6
Mehmet <mehmet.mhy@gmail.com>
2023-11-24 23:18:29 -0700

    updated model.py to support None parameters

--- model.py
+++ model.py
@@ -10,9 +10,32 @@ import argparse
 import uuid
 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "src"))
-import hf
 import logger
 import pointer
+
+def float_or_none(value):
+    if value.lower() == 'none':
+        return None
+    try:
+        return float(value)
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"{value} must be a floating point number or 'None'")
+    
+def int_or_none(value):
+    if value.lower() == 'none':
+        return None
+    try:
+        return int(value)
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"{value} must be an int number or 'None'")
+    
+def str_or_none(value):
+    if value.lower() == 'none':
+        return None
+    try:
+        return str(value)
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"{value} must be a string or 'None'")
 
 parser = argparse.ArgumentParser(description='run llm model hosted on HuggingFace')
 
@@ -21,18 +44,18 @@ November 21, 2023
 The default values and help values for most of these parameters were taken directly from huggingface documentation:
 https://huggingface.co/docs/transformers/main_classes/text_generation#transformers.GenerationConfig
 """
-parser.add_argument('--max_length', type=int, default=20, help='The maximum length the generated tokens can have. Corresponds to the length of the input prompt + max_new_tokens. Its effect is overridden by max_new_tokens, if also set.')
-parser.add_argument('--temperature', type=float, default=1.0, help='The value used to modulate the next token probabilities.')
-parser.add_argument('--top_k', type=int, default=50, help='The number of highest probability vocabulary tokens to keep for top-k-filtering.')
-parser.add_argument('--top_p', type=float, default=1.0, help='If set to float < 1, only the smallest set of most probable tokens with probabilities that add up to top_p or higher are kept for generation.')
-parser.add_argument('--num_return_sequences', type=int, default=1, help='The number of independently computed returned sequences for each element in the batch.')
+parser.add_argument('--max_length', type=int_or_none, default=20, help='The maximum length the generated tokens can have. Corresponds to the length of the input prompt + max_new_tokens. Its effect is overridden by max_new_tokens, if also set.')
+parser.add_argument('--temperature', type=float_or_none, default=1.0, help='The value used to modulate the next token probabilities.')
+parser.add_argument('--top_k', type=int_or_none, default=50, help='The number of highest probability vocabulary tokens to keep for top-k-filtering.')
+parser.add_argument('--top_p', type=float_or_none, default=1.0, help='If set to float < 1, only the smallest set of most probable tokens with probabilities that add up to top_p or higher are kept for generation.')
+parser.add_argument('--num_return_sequences', type=int_or_none, default=1, help='The number of independently computed returned sequences for each element in the batch.')
 
-parser.add_argument('--uuid', type=str, default=str(uuid.uuid4()), help='The UUID for the collection')
-parser.add_argument('--prompt', type=str, default="Hello World", help='Text prompt for the LLM model to respond too')
-parser.add_argument('--model', type=str, default="", help='Huggingface repo/path to LLM model')
-parser.add_argument('--device', type=str, default="", help='Device to run the model on, this can be "cpu" or "cuda:N"')
-parser.add_argument('--dtype', type=str, default="bfloat16", help="A tensor's data type, this will effect the overall accuracy and hardware performance for a model")
-parser.add_argument('--framework', type=str, default=None, help="Perfered framework to run LLM model on (huggingface, LLM-VM, etc)")
+parser.add_argument('--uuid', type=str_or_none, default=str(uuid.uuid4()), help='The UUID for the collection')
+parser.add_argument('--prompt', type=str_or_none, default="Hello World", help='Text prompt for the LLM model to respond too')
+parser.add_argument('--model', type=str_or_none, default="", help='Huggingface repo/path to LLM model')
+parser.add_argument('--device', type=str_or_none, default="", help='Device to run the model on, this can be "cpu" or "cuda:N"')
+parser.add_argument('--dtype', type=str_or_none, default="bfloat16", help="A tensor's data type, this will effect the overall accuracy and hardware performance for a model")
+parser.add_argument('--framework', type=str_or_none, default=None, help="Perfered framework to run LLM model on (huggingface, LLM-VM, etc)")
 
 # signal handler
 def signal_handler(signum, frame):
