5e1713aded4d24a493c91c12ffcc281c3d35841a
Mehmet <mehmet.mhy@gmail.com>
2023-11-26 23:24:03 -0700

    updated columns.py to save final results to a CSV file

--- tools/columns.py
+++ tools/columns.py
@@ -1,6 +1,8 @@
 import json
 import os
 import fnmatch
+import csv
+import time
 
 def read_json(path):
     with open(str(path)) as file:
@@ -21,6 +23,16 @@ def find_json_files(directory, deep_search=False):
             matches.append(os.path.join(root, filename))
     return matches
 
+# https://stackoverflow.com/a/34311071
+def save_csv(filepath, data):
+    filepath = str(filepath)
+    if filepath.endswith(".csv") == False:
+        raise Exception(f"filepath MOST end with .csv")
+    with open(str(filepath), "w") as f:
+        wr = csv.DictWriter(f, delimiter="\t",fieldnames=list(data[0].keys()))
+        wr.writeheader()
+        wr.writerows(data)
+
 # main function calls
 
 reports_path = os.path.join(os.path.abspath(os.path.join(os.getcwd(), os.pardir)), "reports")
@@ -29,6 +41,8 @@ json_files_paths = find_json_files(reports_path)
 
 output = []
 for path in json_files_paths:
+    print(f"Processing file: {path}")
+
     data = read_json(path)
 
     max_gpu_memory_usage = -1
@@ -59,5 +73,7 @@ for path in json_files_paths:
 
     output.append(tmp)
 
-print(json.dumps(output, indent=4))
+csv_filename = f"results_{time.time()}.csv"
+save_csv(csv_filename, output)
+print(f"Created CSV file: {csv_filename}")
 
